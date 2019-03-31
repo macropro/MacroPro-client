0e6bafd4424a5435eefe90c10158d5b27e0f084c
Jason Clifton <j.clifton1473@gmail.com>
2019-03-31 04:01:43 -0600

    added React Router initial route set up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" render={() => <h1>Hello world</h1>} />
+        </Switch>
+
+      </BrowserRouter>
     );
   }
 }
