492c413f9694db38830a43b720d13dcaa7bbd8a2
Jason Clifton <j.clifton1473@gmail.com>
2019-04-01 16:17:10 -0600

    switched to hooks

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" render={() => <h1>Hello world</h1>} />
-        </Switch>
+const app = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" render={() => <h1>Hello world</h1>} />
+      </Switch>
 
-      </BrowserRouter>
-    );
-  }
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default app;
